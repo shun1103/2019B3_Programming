df00b37c2697a8583091a9242e032d677f17ab8e
shun1103 <s.k.baseball-lover@keio.jp>
2019-03-04 14:06:06 +0900

    15問目の回答を追加

---
+++ p1_python演習/my_answer/ex15.py
@@ -0,0 +1,12 @@
+import numpy as np
+from ex13 import tf
+from ex14 import idf
+docs = [["リンゴ", "リンゴ"], ["リンゴ", "レモン"], ["レモン", "ミカン"]]
+terms = ["リンゴ", "レモン", "ミカン"]
+
+matrix = np.zeros((3, 3))
+for i in range(3):
+    for j in range(3):
+        matrix[j][i] = tf(terms[i], docs[j]) * idf(terms[i], docs)
+
+print(matrix)
